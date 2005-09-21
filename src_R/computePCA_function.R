518f87f02fce1add47b983a0167c3a16ee3fbf32
Harris Michael <harrismic>
2005-09-21 14:34:54 +0000

    Change function names
    SVN-Revision: 6354

--- src_R/computePCA_function.R
+++ src_R/computePCA_function.R
@@ -7,14 +7,15 @@
 
 # Filtering option 1 - 95%Percentile / 5%Percentile >= 2 or 3
 
-    GeneFilter1 <- function(datmat, cons=3, ...) {
+    GeneFilterWithFC <- function(datmat, cons=3, ...) {
     tmp <- apply(datmat, 1, quantile, c(.05, .95) , na.rm=TRUE)
     sum(2^(tmp[2,]-tmp[1,])>cons) 
-    datmat2 <- datmat[2^(tmp[2,]-tmp[1,])>cons, ]   
+    filteredDataMatrix <- datmat[2^(tmp[2,]-tmp[1,])>cons, ]   
+    return(filteredDataMatrix)
     }   
 
-    computePCA1 <- function(datmat, filterParam) {
-    filteredDataMatrix <- GeneFilter1(datmat, filterParam)
+    computePCAWithFC <- function(datmat, filterParam) {
+    filteredDataMatrix <- GeneFilterWithFC(datmat, filterParam)
     tmp <- apply(is.na(filteredDataMatrix),1,sum,na.rm=T)
     dpca <- prcomp(t(filteredDataMatrix[tmp==0,]))
     return(dpca)
@@ -23,15 +24,15 @@
 # Filtering option 2 - The variance of each gene over all samples was calculated and the top  
 # high variance genes selected
 
-    GeneFilter2 <- function(datmat, cons=0.70) {
+    GeneFilterWithVariance <- function(datmat, cons=0.70) {
     tmp1 <- apply(datmat, 1, var, na.rm=TRUE)
     tmp2 <- quantile(tmp1,cons)
-    datmat2 <- datmat[tmp1>=tmp2,]
-    return(datmat2)
+    filteredDataMatrix <- datmat[tmp1>=tmp2,]
+    return(filteredDataMatrix)
     }
     
-    computePCA2 <- function(datmat, filterParam) {
-    filteredDataMatrix <- GeneFilter2(datmat, filterParam)
+    computePCAWithVariance <- function(datmat, filterParam) {
+    filteredDataMatrix <- GeneFilterWithVariance(datmat, filterParam)
     tmp <- apply(is.na(filteredDataMatrix),1,sum,na.rm=T)
     dpca <- prcomp(t(filteredDataMatrix[tmp==0,]))
     return(dpca)
