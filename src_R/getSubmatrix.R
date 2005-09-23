8ae33369378fb494b6d32930779ca981c64e5f81
Harris Michael <harrismic>
2005-09-23 16:13:30 +0000

    Add get submatrixbased on one group sample ids
    SVN-Revision: 6386

--- src_R/getSubmatrix.R
+++ src_R/getSubmatrix.R
@@ -6,19 +6,28 @@
 ########################
 
 # Generate submatrix based on sample IDs
-getSubmatrix <- function(datmat, grp1ids, grp2ids) {
+getSubmatrix.twogrps <- function(datmat, grp1ids, grp2ids) {
 	allids <- dimnames(datmat)[[2]]
     Submatrix <- cbind(datmat[,allids%in%grp1ids],datmat[,allids%in%grp2ids])
     return(Submatrix)
 }
 
-# Submatrix <- getSubmatrix(datmat, grp1ids, grp2ids)  
+# Submatrix <- getSubmatrix.twogrps(datmat, grp1ids, grp2ids)  
+
+# Generate submatrix based on one group sample IDs
+getSubmatrix.onegrp <- function(datmat, grpids) {
+	allids <- dimnames(datmat)[[2]]
+    Submatrix <- datmat[,allids%in%grpids]
+    return(Submatrix)
+}
+
+# Submatrix <- getSubmatrix.onegrp(datmat, grpids)
   
 # Generate submatrix based on reporters
 getSubmatrix.rep <- function(datmat, rep.ids) {
 	allrep.ids <- dimnames(datmat)[[1]]
-    datmat.rep <- datmat[allrep.ids%in%rep.ids,]
-    return(datmat.rep)
+    Submatrix.rep <- datmat[allrep.ids%in%rep.ids,]
+    return(Submatrix.rep)
 }
 
-# datmat.rep <- getSubmatrix.rep(datmat, rep.ids)  
+# Submatrix.rep <- getSubmatrix.rep(datmat, rep.ids)  
