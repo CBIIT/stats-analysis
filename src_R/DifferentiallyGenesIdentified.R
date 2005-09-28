991d928de18023c514f2da2b6e33efd135ed04fd
Harris Michael <harrismic>
2005-09-28 14:37:41 +0000

    Removed datmat paramter from functions since it wasn't used.
    SVN-Revision: 6399

--- src_R/DifferentiallyGenesIdentified.R
+++ src_R/DifferentiallyGenesIdentified.R
@@ -6,23 +6,23 @@
 
 # Identify differentially expressed genes based on absolute fold change>=2 and pvalue<=0.001
 
-mydiferentiallygenes <- function(datmat,raw.result,cons1=2, cons2=0.001) {
+mydiferentiallygenes <- function(raw.result,cons1=2, cons2=0.001) {
 sel <- (raw.result$fc >= cons1) & (raw.result$pval <=cons2)
 filtered.result <- raw.result[sel,]
 return(filtered.result)
 }
 
 # Call function
-# filtered.result <- mydiferentiallygenes(datmat,raw.result,2,0.001)
+# filtered.result <- mydiferentiallygenes(raw.result,2,0.001)
 
 ###############################################################################################################
 # Identify differentially expressed genes based on absolute fold change>=2 and adjustP<=0.1
 
-mydiferentiallygenes.adjustP <- function(datmat, adjust.result, cons1=2, cons2=0.1) {
+mydiferentiallygenes.adjustP <- function(adjust.result, cons1=2, cons2=0.1) {
 sel <- (adjust.result$fc >= cons1) & (adjust.result$adjustP <=cons2)
 filtered.result <- adjust.result[sel,]
 return(filtered.result)
 }
 
 # Call function
-# filtered.result <- mydiferentiallygenes.adjustP(datmat,adjust.result,2,0.1)
+# filtered.result <- mydiferentiallygenes.adjustP(adjust.result,2,0.1)
