2d157023abbbe6d50f053e94250ba0f30ff0dca0
Harris Michael <harrismic>
2005-10-07 17:09:53 +0000

    New enumeration.
    SVN-Revision: 6427

---
+++ src/gov/nih/nci/caintegrator/enumeration/DistanceMatrixType.java
@@ -0,0 +1,4 @@
+package gov.nih.nci.caintegrator.enumeration;
+
+public enum DistanceMatrixType {Correlation, Euclidean }
+
---
+++ src/gov/nih/nci/caintegrator/enumeration/LinkageMethodType.java
@@ -0,0 +1,4 @@
+package gov.nih.nci.caintegrator.enumeration;
+
+public enum LinkageMethodType { Average, Single, Complete };
+
