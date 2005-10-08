211c41c968d88c1dab410d8218713e7f8ef072ca
Himanso Sahni <Himanso.Sahni@fda.hhs.gov>
2005-10-08 08:48:49 +0000

    Initial Check in
    SVN-Revision: 6433

---
+++ src/gov/nih/nci/caintegrator/enumeration/BooleanOperator.java
@@ -0,0 +1,16 @@
+package gov.nih.nci.caintegrator.enumeration;
+
+/**
+ * @author sahnih
+ * This enum describes various operators that
+ * are used through out the caIntegrator to formulate criteria.
+ *
+ */
+public enum BooleanOperator {
+	GT, //greater than
+	LT, //less than
+	EQ, //equals
+	NE, //not equal to
+	GE, //greater than or equal to
+	LE, //less than or equal to
+}
