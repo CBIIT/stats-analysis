cf4c88d106ebf52d4dace12bbf4bc4e322f117a1
Harris Michael <harrismic>
2005-11-14 22:52:02 +0000

    New plot class for principal component analysis.
    SVN-Revision: 6497

---
+++ src/gov/nih/nci/caintegrator/ui/graphing/chart/plot/PrincipalComponentAnalysisPlot.java
@@ -0,0 +1,21 @@
+package gov.nih.nci.caintegrator.ui.graphing.chart.plot;
+
+public class PrincipalComponentAnalysisPlot {
+
+	public static enum PCAcolorByType {Disease, DiseaseGrade, Gender };
+	public static enum PCAcomponent { PC1, PC2, PC3 };
+	
+	public PrincipalComponentAnalysisPlot(PCAcomponent component1, PCAcomponent component2) {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
