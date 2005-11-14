b2c43a626137795ebf6c08272a48edf01aeb925c
Harris Michael <harrismic>
2005-11-14 22:51:08 +0000

    New class to hold plotting data for PCA graphs.
    SVN-Revision: 6496

---
+++ src/gov/nih/nci/caintegrator/ui/graphing/data/principalComponentAnalysis/PrincipalComponentAnalysisDataPoint.java
@@ -0,0 +1,11 @@
+package gov.nih.nci.caintegrator.ui.graphing.data.principalComponentAnalysis;
+
+public class PrincipalComponentAnalysisDataPoint {
+
+	public PrincipalComponentAnalysisDataPoint() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	
+}
