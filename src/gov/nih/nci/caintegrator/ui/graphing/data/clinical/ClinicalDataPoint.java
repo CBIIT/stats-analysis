84d839e2dfc9b7712d6ac96585827d9e03a62aba
Harris Michael <harrismic>
2005-11-21 19:24:19 +0000

    Added more functionality for clinical plots.
    SVN-Revision: 6524

--- src/gov/nih/nci/caintegrator/ui/graphing/data/clinical/ClinicalDataPoint.java
+++ src/gov/nih/nci/caintegrator/ui/graphing/data/clinical/ClinicalDataPoint.java
@@ -8,21 +8,40 @@ public class ClinicalDataPoint {
 	private String patientId;
 	private double ageAtDx;
 	private String treatment;  
+	private String diseaseName;
 	private double neurologicalAssessment;
 	private double survival;
 	private PatientGenderType gender;
+	private int diseaseGrade;
 	
 	public ClinicalDataPoint(String patientId) {
 	  this.patientId = patientId;
 	}
 
-	public ClinicalDataPoint(String patientId,PatientGenderType gender,  double ageAtDx, String treatment, double neurologicalAssessment, double survival) {
+	public ClinicalDataPoint(String patientId,PatientGenderType gender, String diseaseName, int diseaseGrade, double ageAtDx, String treatment, double neurologicalAssessment, double survival) {
 	  this.patientId = patientId;
 	  this.gender = gender;
 	  this.ageAtDx = ageAtDx;
 	  this.treatment = treatment;
 	  this.neurologicalAssessment = neurologicalAssessment;
 	  this.survival = survival;
+	  this.diseaseName = diseaseName;
+	  this.diseaseGrade = diseaseGrade;
+	}
+	
+	/**
+	 * Return the value of the specified clinical factor. If an invalid component is
+	 * specified, the function will return Double.NEGATIVE_INFINITY;
+	 * @param component
+	 * @return
+	 */
+	public double getFactorValue(ClinicalFactorType factor) {
+	  switch(factor) {
+	  case AgeAtDx: return getAgeAtDx();
+	  case NeurologicalAssessment: return getNeurologicalAssessment();
+	  case Survival: return getSurvival();
+	  }
+	  return Double.NEGATIVE_INFINITY;
 	}
 
 	public double getAgeAtDx() {
@@ -68,5 +87,20 @@ public class ClinicalDataPoint {
 	public void setGender(PatientGenderType gender) {
 		this.gender = gender;
 	}
-	
+
+	public String getDiseaseName() {
+		return diseaseName;
+	}
+
+	public void setDiseaseName(String diseaseName) {
+		this.diseaseName = diseaseName;
+	}
+
+	public int getDiseaseGrade() {
+		return diseaseGrade;
+	}
+
+	public void setDiseaseGrade(int diseaseGrade) {
+		this.diseaseGrade = diseaseGrade;
+	}
 }
