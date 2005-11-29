e42922966c9c188aeae708a3ac873cf9a34adc71
bauerd <bauerd>
2005-11-29 23:48:06 +0000

    *** empty log message ***
    SVN-Revision: 6546

--- src/gov/nih/nci/caintegrator/enumeration/DistanceMatrixType.java
+++ src/gov/nih/nci/caintegrator/enumeration/DistanceMatrixType.java
@@ -3,15 +3,6 @@ package gov.nih.nci.caintegrator.enumeration;
 import java.io.Serializable;
 
 public enum DistanceMatrixType implements Serializable {
-	Correlation{ 
-        public String toString() 
-        { 
-            return "Correlation";
-        } },
-	Euclidean{ 
-        public String toString() 
-        { 
-            return "Euclidean";
-        } };
-	}
+	Correlation, Euclidean;
+}
 
