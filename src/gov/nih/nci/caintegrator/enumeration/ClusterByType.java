24328da4cd078026a49f58df1d69e75ad06f0300
Kevin Rosso <Kevin.Rosso@fda.hhs.gov>
2005-11-30 14:43:37 +0000

    toString ommitted for Unix
    SVN-Revision: 6547

--- src/gov/nih/nci/caintegrator/enumeration/ClusterByType.java
+++ src/gov/nih/nci/caintegrator/enumeration/ClusterByType.java
@@ -3,15 +3,6 @@ package gov.nih.nci.caintegrator.enumeration;
 import java.io.Serializable;
 
 public enum ClusterByType implements Serializable{
-	Genes{ 
-        public String toString() 
-        { 
-            return "genes";
-        } },
-	Samples{ 
-        public String toString() 
-        { 
-            return "samples";
-        } };
+	Genes, Samples;
 }
 
