c5e32a28319fc80dd90e297cda487db5424be3b6
Harris Michael <harrismic>
2005-12-06 20:17:50 +0000

    Changed enum names.
    SVN-Revision: 6574

--- src/gov/nih/nci/caintegrator/enumeration/GenderType.java
+++ src/gov/nih/nci/caintegrator/enumeration/GenderType.java
@@ -3,9 +3,9 @@ package gov.nih.nci.caintegrator.enumeration;
 import java.awt.Color;
 
 public enum GenderType {
-Male { Color getColor() { return Color.BLUE; }}, 
-Female {Color getColor() { return Color.PINK; }},
-Unknown {Color getColor() { return Color.DARK_GRAY; }};
+M { Color getColor() { return Color.BLUE; }}, 
+F {Color getColor() { return Color.PINK; }},
+O {Color getColor() { return Color.DARK_GRAY; }};
 
 abstract Color getColor();
 
