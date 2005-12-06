c82b67c8e7c45eebf537406085f64349505aa996
Harris Michael <harrismic>
2005-12-06 18:11:51 +0000

    Added getColor() method.
    SVN-Revision: 6571

--- src/gov/nih/nci/caintegrator/enumeration/GenderType.java
+++ src/gov/nih/nci/caintegrator/enumeration/GenderType.java
@@ -1,7 +1,12 @@
 package gov.nih.nci.caintegrator.enumeration;
 
+import java.awt.Color;
+
 public enum GenderType {
-Male, 
-Female, 
-Unknown
+Male { Color getColor() { return Color.BLUE; }}, 
+Female {Color getColor() { return Color.PINK; }},
+Unknown {Color getColor() { return Color.DARK_GRAY; }};
+
+abstract Color getColor();
+
 }
