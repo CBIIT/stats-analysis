5831e066dd3fde66ad3b38c6dc484b09530281e6
Michael Holck <mholck@scenpro.com>
2006-09-20 20:51:36 +0000

    Added index attribute and getter and setter methods
    SVN-Revision: 6888

--- src/gov/nih/nci/caintegrator/studyQueryService/dto/FindingCriteriaDTO.java
+++ src/gov/nih/nci/caintegrator/studyQueryService/dto/FindingCriteriaDTO.java
@@ -13,6 +13,7 @@ public abstract class FindingCriteriaDTO {
     abstract public FindingsHandler getHandler();
 
     public AnnotationCriteria AnnotationCriteria;
+    public int index;
 
     public FindingCriteriaDTO(){ }
 
@@ -23,5 +24,13 @@ public abstract class FindingCriteriaDTO {
     public void setAnnotationCriteria(AnnotationCriteria annotationCriteria) {
         AnnotationCriteria = annotationCriteria;
     }
+
+	public int getIndex() {
+		return index;
+	}
+
+	public void setIndex(int index) {
+		this.index = index;
+	}
 
 }
