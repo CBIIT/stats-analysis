dbf0313890acbd11cbf86cc996a81f2ddff94ce2
no_author <no_author@no_author.cvs2svn>
2006-09-26 16:46:51 +0000

    This commit was manufactured by cvs2svn to create tag 'QA_26SEP06_001'.
    SVN-Revision: 6908

---
+++ QA_26SEP06_001/src/gov/nih/nci/caintegrator/studyQueryService/germline/SNPAnnotationCriteriaHandler.java
@@ -0,0 +1,239 @@
+package gov.nih.nci.caintegrator.studyQueryService.germline;
+
+import org.hibernate.Session;
+import org.hibernate.Query;
+
+import java.util.*;
+import java.text.MessageFormat;
+
+import gov.nih.nci.caintegrator.studyQueryService.dto.annotation.AnnotationCriteria;
+import gov.nih.nci.caintegrator.studyQueryService.dto.annotation.PhysicalPositionCriteria;
+import gov.nih.nci.caintegrator.studyQueryService.dto.germline.PanelCriteria;
+import gov.nih.nci.caintegrator.util.HQLHelper;
+import gov.nih.nci.caintegrator.domain.annotation.snp.bean.SNPAnnotation;
+
+/**
+ * Author: Ram Bhattaru
+ * Date:   Jul 5, 2006
+ * Time:   4:52:03 PM
+ */
+public class SNPAnnotationCriteriaHandler {
+    /**
+     * This method converts each of the annotation specified in the criteria in
+     * to AnnotationIDs.  In the current release only PanelCriteria, PositionCriteria
+    *  and SnpIdentifier are only supported.  CytobandCriteria,   GenePathways,
+    *  GeneOntology are not supported in thie release.
+    *  criteria
+     *
+     * @param annotCrit
+     * @param session
+     * @return Set of SNPAnnotation IDs
+     */
+
+    public static Set<String> handle(AnnotationCriteria annotCrit,  Session session)
+    throws Exception {
+
+        Set<String> allSNPAnnotIDs = null;
+        if (annotCrit == null) throw new Exception ("Annotation criteria can not be null");
+
+        List<SNPAnnotation> annotObjs = getSNPAnnotations(annotCrit, session);
+
+        if (annotObjs != null) {
+            allSNPAnnotIDs = new HashSet();
+            /* add ids from this annotObjs list to results i.e allSNPAnnotIDs */
+            for (int i = 0; i < annotObjs.size(); i++) {
+                SNPAnnotation snpAnnotation =  annotObjs.get(i);
+                allSNPAnnotIDs.add(snpAnnotation.getId());
+            }
+            System.out.println("Total SNPAnnotations Retrieved: " + allSNPAnnotIDs.size());
+        }
+        return allSNPAnnotIDs;
+    }
+
+    /**
+     * This method retrieves ANPAnnotations based on AnnotationCriteria passed in.
+     * In the current release AnnotationCriteria only PanelCriteria, PositionCriteria
+     * and SnpIdentifier are supported.  CytobandCriteria,   GenePathways,
+     * GeneOntology will be supported in later releases.
+     *
+     * @param annotCrit
+     * @param session
+     * @return List of SNPAnnotation objects
+     */
+
+    public static List<SNPAnnotation> getSNPAnnotations(AnnotationCriteria annotCrit,  Session session)
+    throws Exception {
+
+        List<SNPAnnotation> annotObjs = new ArrayList<SNPAnnotation>();
+        if (annotCrit == null) throw new Exception ("Annotation criteria can not be null");
+
+        PhysicalPositionCriteria poistionCrit = annotCrit.getPhysicalPositionCriteria();
+        Collection<String> dbSNPIdentifiers = annotCrit.getSnpIdentifiers();
+        Collection<String> geneSymbols = annotCrit.getGeneSymbols();
+
+        HashMap params = new HashMap();  // used to hold HQL parameters
+
+        PanelCriteria panelCrit = annotCrit.getPanelCriteria();
+        /* at least one other annotation should be specified when panelCriteria
+           is specified in the AnnotationCriteria.  Otherwise throw an exception */
+        if ((panelCrit != null) && (poistionCrit == null && dbSNPIdentifiers == null) )
+          throw new Exception("At least one other annotation should be specified with panelCriteria ");
+
+        /* 1.  Handle SNPAnnotation itself */
+        String annotHSQL = new String(" FROM SNPAnnotation s WHERE {0} ");
+        StringBuffer snpAnnotHSQL = new StringBuffer(annotHSQL);
+        String geneSymbolCond = "";
+        if (geneSymbols != null && geneSymbols.size() > 0) {
+            geneSymbolCond = " s.geneBiomarkerCollection.hugoGeneSymbol IN (:geneSymbols) OR ";
+            params.put("geneSymbols", geneSymbols);
+        }
+
+        /* 2 Handle PhysicalPositionCriteria */
+        if (poistionCrit != null) {
+            handlePositionCriteria(poistionCrit, snpAnnotHSQL, params);
+        }
+
+        /* 3 Handle CytobandCriteria, GeneOntology, GenePathway */
+        if ((annotCrit.getCytobandCriteria() != null) ||
+           (annotCrit.getGeneOntology() != null) || (annotCrit.getGenePathways() != null) )
+           throw new RuntimeException (" This method is not implelemted for now: ");
+
+        /* 4 Handle SNPIdentifiers */
+        dbSNPIdentifiers = annotCrit.getSnpIdentifiers();
+        if (dbSNPIdentifiers !=  null && dbSNPIdentifiers.size() > 0) {
+            String tmp = new String(" s.dbsnpId IN (:dbSnps ) OR ");
+            params.put("dbSnps", dbSNPIdentifiers);
+            snpAnnotHSQL.append(tmp);
+        }
+
+        /* 5 Remove any trailing AND / OR and append AND */
+        String paramReplacedHql = MessageFormat.format(snpAnnotHSQL.toString(), new Object[] { geneSymbolCond });
+        String interHSQL = HQLHelper.removeTrailingAND(new StringBuffer(paramReplacedHql));
+        String finalWithWhereHSQL = HQLHelper.removeTrailingOR(new StringBuffer(interHSQL));
+        String finalWithoutWhereHQL = HQLHelper.removeTrailingToken(new StringBuffer(finalWithWhereHSQL), "WHERE");
+
+        /* 6  Include PanelCritera */
+        String panelBasedSNPAnnotCrit = handlePanelCriteria(panelCrit, params);
+        String finalHQL = new String("");
+        if (panelBasedSNPAnnotCrit.length() > 0 ) {
+            /* implies that there is already at least one other criteria (such as PhysicalPosition,
+               dbSNPIdentifiers etc) was metnioned and at the samtime there is also hql for PanelCrit
+               is included.  So add panel criteria to the above SNPAnnotation criteria as subselect with
+               preciding OR operator */
+            finalHQL = new StringBuffer(finalWithoutWhereHQL).append(" OR ").append(panelBasedSNPAnnotCrit).toString();
+        } else {
+            finalHQL = new StringBuffer(finalWithoutWhereHQL).toString();
+        }
+/*
+        if (panelBasedSNPAnnotCrit.length() > 0) {
+            // append the above SNPAnnotation criteria as sub select
+           panelBasedSNPAnnotCrit.append(" ( " + finalHQL + " )");
+        }
+        else {
+           panelBasedSNPAnnotCrit.append(finalHQL);
+        }
+*/
+        if (finalHQL.indexOf(" WHERE ") == -1) {
+            /* means no WHERE clause at all which means selecting entire table.  So return no anotations
+             so that the annotaiton clause does not need to be included in final Finding HQL */
+            /* TODO: this way of returning "null" in general is a bad parctice.  This is done as
+               temporary fix.  It will be re-factored later --  Ram 08/23/06
+            */
+            return null;
+        }
+        Query q = session.createQuery(finalHQL.toString());
+        HQLHelper.setParamsOnQuery(params, q);
+        annotObjs =  q.list();
+        return annotObjs;
+    }
+
+    public static Boolean isAnnotationCriteriaPresent(AnnotationCriteria annotCrit) {
+        boolean criteriaPresent = false;
+
+        PhysicalPositionCriteria poistionCrit = annotCrit.getPhysicalPositionCriteria();
+        Collection<String> dbSNPIdentifiers = annotCrit.getSnpIdentifiers();
+        Collection<String> geneSymbols = annotCrit.getGeneSymbols();
+        PanelCriteria panelCrit = annotCrit.getPanelCriteria();
+        /* TODO: when the following are implemented in  the next release pl take care of the below:
+        String[] geneOntology = annotCrit.getGeneOntology();
+        String[] genePathways = annotCrit.getGenePathways();
+        CytobandCriteria cytobandCriteria = annotCrit.CytobandCriteria();
+        */
+        if (poistionCrit != null) {
+          String chromosome = poistionCrit.getChromosome();
+          Long startPos = poistionCrit.getStartPosition();
+          Long endPos = poistionCrit.getEndPosition();
+          if ((chromosome != null)) criteriaPresent = true;
+        }
+
+        if (dbSNPIdentifiers !=  null && dbSNPIdentifiers.size() > 0) criteriaPresent = true;
+        if (geneSymbols != null && geneSymbols.size() > 0) criteriaPresent = true;
+        if (panelCrit != null) {
+            String name = panelCrit.getName();
+            String version = panelCrit.getVersion();
+            if (name != null || version != null) criteriaPresent = true;
+        }
+
+        return new Boolean(criteriaPresent);
+    }
+
+
+    private static void handlePositionCriteria(PhysicalPositionCriteria poistionCrit, StringBuffer snpAnnotHSQL, HashMap params) {
+        String chromosome = poistionCrit.getChromosome();
+        Long startPos = poistionCrit.getStartPosition();
+        Long endPos = poistionCrit.getEndPosition();
+
+        if ((chromosome == null) || (startPos == null) || (endPos == null) )
+             throw new RuntimeException("Chromosme, StartPosition, EndPosition are required ");
+        String tmp = new String(" (s.chromosomeName=:chr AND ( s.chromosomeLocation  BETWEEN :start AND :end )) OR ");
+        params.put("chr", chromosome);
+        params.put("start", startPos);
+        params.put("end", endPos);
+        snpAnnotHSQL.append(tmp);
+    }
+
+    /**
+     * This method call implies that there is already at least one other criteria (such as PhysicalPosition,
+     * dbSNPIdentifiers etc) was metnioned.
+     * @param panelCrit
+     * @param params
+     * @return hql containg panelCriteria (or empty string if no PanelCrit is specified)
+     */
+    private static String handlePanelCriteria(PanelCriteria panelCrit,  HashMap params ) {
+
+            StringBuffer emptyBuffer = new StringBuffer("");
+            if (panelCrit == null) return emptyBuffer.toString();
+
+            String name = panelCrit.getName();
+            String version = panelCrit.getVersion();
+            if (name == null & version == null) return emptyBuffer.toString();
+
+            StringBuffer snpAnnotIDsCrit = new StringBuffer("");
+            boolean panelCritAdded = false;
+            StringBuffer panelClause = new StringBuffer("");
+            if (name != null) {
+              panelClause.append(" sp.name = :name AND ");
+              params.put( "name", name);
+              panelCritAdded = true;
+            }
+            if (version != null) {
+              panelClause.append(" sp.version = :version ");
+              params.put( "version", version);
+              panelCritAdded = true;
+            }
+
+            if (panelCritAdded) {
+                snpAnnotIDsCrit.append( " s.id IN ( SELECT sa.snpAnnotation.id " +
+                                        " FROM SNPAssay sa JOIN sa.snpPanel sp WHERE ");
+                snpAnnotIDsCrit.append(panelClause);
+            }
+
+            StringBuffer finalHQL = new StringBuffer("");
+            if (snpAnnotIDsCrit.length() > 0) {
+                String interHQL = HQLHelper.removeTrailingToken(snpAnnotIDsCrit, "AND");
+                finalHQL = new StringBuffer(interHQL).append(" ) ");
+            }
+
+            return finalHQL.toString();
+        }
+}
---
+++ QA_26SEP06_001/src/gov/nih/nci/caintegrator/studyQueryService/test/germline/GenotypeFindingTest.java
@@ -0,0 +1,330 @@
+package gov.nih.nci.caintegrator.studyQueryService.test.germline;
+
+import gov.nih.nci.caintegrator.domain.annotation.snp.bean.SNPAnnotation;
+import gov.nih.nci.caintegrator.domain.finding.bean.Finding;
+import gov.nih.nci.caintegrator.domain.finding.bean.SpecimenBasedMolecularFinding;
+import gov.nih.nci.caintegrator.domain.finding.variation.germline.bean.GenotypeFinding;
+import gov.nih.nci.caintegrator.domain.study.bean.DNASpecimen;
+import gov.nih.nci.caintegrator.studyQueryService.dto.annotation.AnnotationCriteria;
+import gov.nih.nci.caintegrator.studyQueryService.dto.annotation.PhysicalPositionCriteria;
+import gov.nih.nci.caintegrator.studyQueryService.dto.germline.GenotypeFindingCriteriaDTO;
+import gov.nih.nci.caintegrator.studyQueryService.dto.germline.PanelCriteria;
+import gov.nih.nci.caintegrator.studyQueryService.dto.germline.AnalysisGroupCriteria;
+import gov.nih.nci.caintegrator.studyQueryService.dto.study.PopulationCriteria;
+import gov.nih.nci.caintegrator.studyQueryService.dto.study.StudyCriteria;
+import gov.nih.nci.caintegrator.studyQueryService.dto.study.StudyParticipantCriteria;
+import gov.nih.nci.caintegrator.studyQueryService.germline.FindingsManager;
+import gov.nih.nci.caintegrator.studyQueryService.germline.FindingsHandler;
+import gov.nih.nci.caintegrator.util.ArithematicOperator;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Author: Ram Bhattaru
+ * Date:   Jul 6, 2006
+ * Time:   5:53:05 PM
+ */
+
+public class GenotypeFindingTest extends CGEMSTest {
+   // protected AnnotationCriteria annotCrit;
+    private GenotypeFindingCriteriaDTO gfDTO;
+   // protected static int TOTAL_FINDINGS = 0;
+
+
+    public void setUp() throws Exception {
+        super.setUp();
+        gfDTO = new GenotypeFindingCriteriaDTO();
+        gfDTO.setAnnotationCriteria(annotCrit);
+        gfDTO.setStudyParticipantCriteria(spCrit);
+    }
+
+    public void testAll() {
+        super.testAll();
+    }
+
+
+
+    private void setUpGenotypeCrit() {
+        //gfDTO.setQualityScore(new Float(0.50), ArithematicOperator.LT);
+        gfDTO.setQcStatus("QC+");
+    }
+    protected void setUpSNPPhysicalPositionCrit() {
+        PhysicalPositionCriteria ppc = new PhysicalPositionCriteria();
+/*
+        ppc.setChromosome("8");
+        ppc.setStartPosition(new Integer(76065000));  // 76065158
+        ppc.setEndPosition(new Integer(76075000));    // should give 4 GenotypeFindings
+
+*/
+        ppc.setChromosome("8");
+        ppc.setStartPosition(new Long(76065000));  // 76065158
+        ppc.setEndPosition(new Long(96100000));    // should give 4 GenotypeFindings
+
+        /*
+        ppc.setChromosome("X");
+        ppc.setStartPosition(new Long(2693518));   // entire chromosome
+        ppc.setEndPosition(new Long(154493116));
+        */
+
+        annotCrit.setPhysicalPositionCriteria(ppc);
+
+    }
+    private void setUpSNPHugoGeneSymbolCrit() {
+       Collection<String> geneSymbols = new ArrayList();
+       //geneSymbols.add()
+       annotCrit.setGeneSymbols(geneSymbols);
+    }
+    protected void setUpDBSnpCrit() {
+        Collection<String> dbSNPIds = new ArrayList<String>();
+        //dbSNPIds.add("rs10215692");
+        //dbSNPIds.add("rs10216611");
+        //dbSNPIds.add("rs10170496");
+        dbSNPIds.add("rs7867544");
+        //dbSNPIds.add("rs1160166");
+        //dbSNPIds.add("rs10504944");
+        annotCrit.setSnpIdentifiers(dbSNPIds );
+    }
+
+    public void testSNPAnnotCrit() {
+        setUpSNPPhysicalPositionCrit();
+        executeGenotypeFindingSearch(0, 60);
+    }
+
+    protected void setUpPanelCrit() {
+        PanelCriteria p = new PanelCriteria();
+        p.setName("HumanHap300");
+        p.setVersion("1.1");
+        annotCrit.setPanelCriteria(p);
+    }
+
+    public void testPanelCrit() {
+        setUpPanelCrit();
+        setUpDBSnpCrit();
+        executeGenotypeFindingSearch(0, 60);
+    }
+
+    public void testDBSnpCrit() {
+        setUpDBSnpCrit();
+        executeGenotypeFindingSearch(0, 60);
+    }
+    public void testPanelAndSNPAnnotCrit() {
+        setUpPanelCrit();
+        setUpSNPPhysicalPositionCrit();
+        executeGenotypeFindingSearch(0, 60);
+    }
+    protected void setUpGeneBiomarkerCrit() {
+        Collection<String> geneSymbols = new ArrayList<String> ();
+        //geneSymbols.add(new String("USP48"));
+        //geneSymbols.add(new String("HSPG2"));
+        geneSymbols.add(new String("MET"));
+
+
+
+        //geneSymbols.add(new String("blimp"));
+/*        geneSymbols.add("MFAP5");
+        geneSymbols.add("BPIL2");
+        geneSymbols.add("IVL");
+        geneSymbols.add("SPRR2A");
+        geneSymbols.add("SPRR2B");
+        geneSymbols.add("CHST8");
+        geneSymbols.add("D4ST1");
+        geneSymbols.add("CALM3");
+        geneSymbols.add("DNAH12");
+        geneSymbols.add("DNAH7");
+        geneSymbols.add("ACHE");
+        geneSymbols.add("APBB1");
+        geneSymbols.add("APOE");
+        geneSymbols.add("MAPK8IP2");
+        geneSymbols.add("INHA");
+        geneSymbols.add("INHBA");
+        geneSymbols.add("INHBB");
+        geneSymbols.add("CAMK2D");
+        geneSymbols.add("CDC2L1");
+        geneSymbols.add("CDC2L2");
+        geneSymbols.add("CHAD");
+        geneSymbols.add("CISH");
+        geneSymbols.add("COVA1");
+        geneSymbols.add("CTGF");
+        geneSymbols.add("CYR61");
+        geneSymbols.add("ESM1");
+        geneSymbols.add("FGFRL1");
+        geneSymbols.add("GAP43");
+        geneSymbols.add("IFNG");
+        geneSymbols.add("IGFBP1");
+        geneSymbols.add("IGFBP3");
+        geneSymbols.add("IGFBP7");
+        geneSymbols.add("LTBP4");
+        geneSymbols.add("MAC30");
+        geneSymbols.add("MORF4L1");
+        geneSymbols.add("NDN");
+        geneSymbols.add("NEDD9");
+        geneSymbols.add("OGFR");
+        geneSymbols.add("OSM");
+        geneSymbols.add("PAPPA2");
+        geneSymbols.add("PLCE1");
+        geneSymbols.add("PRSS11");
+        geneSymbols.add("QSCN6");
+        geneSymbols.add("SHC1");
+        geneSymbols.add("SOCS1");
+        geneSymbols.add("SOCS3");
+        geneSymbols.add("SOCS4");
+        geneSymbols.add("SOCS5");
+        geneSymbols.add("SOCS6");
+        geneSymbols.add("SOCS7");
+        geneSymbols.add("TPSB2");
+        geneSymbols.add("TU3A");
+        geneSymbols.add("VEGFB");
+        geneSymbols.add("WISP1");
+        geneSymbols.add("WISP3");
+        geneSymbols.add("PPP1R9B");
+        geneSymbols.add("HAO1");
+        geneSymbols.add("HAO2");
+
+        geneSymbols.add("ADORA1");
+        geneSymbols.add("ADORA2A");
+        geneSymbols.add("ADORA2B");
+        geneSymbols.add("ADORA3");
+        geneSymbols.add("ADRA1B");
+        geneSymbols.add("ADRA1D");
+        geneSymbols.add("ADRA2B");
+        geneSymbols.add("ADRA2C");
+        geneSymbols.add("ADRB1");
+        geneSymbols.add("ADRB3");
+        geneSymbols.add("AGTR1");
+        geneSymbols.add("AGTR2");
+        geneSymbols.add("AGTRL1");
+        geneSymbols.add("AVPR1B");
+        geneSymbols.add("BDKRB1");
+        geneSymbols.add("BDKRB2");
+        geneSymbols.add("BLR1");
+        geneSymbols.add("BRS3");
+        geneSymbols.add("C3AR1");
+        geneSymbols.add("CCBP2");
+        geneSymbols.add("CCKBR");
+        geneSymbols.add("CCR1");
+        geneSymbols.add("CCR3");
+        geneSymbols.add("CCR4");
+        geneSymbols.add("CCR5");
+        geneSymbols.add("CCR8");
+        geneSymbols.add("CCR9");
+        geneSymbols.add("CCRL1");
+        geneSymbols.add("CCRL2");
+        geneSymbols.add("CHRM1");
+
+*/
+        annotCrit.setGeneSymbols(geneSymbols);
+
+    }
+    public void setUpStudyParticipantCrit() {
+
+        /*  Specify StudyParticipantCriteria's Associations Criteria */
+        setUpStudyCriteria();
+        setUpPopulationCriteria();
+
+        /*  Specify StudyParticipant Attribute Criteria itself */
+        //setUpStudyParticipantAttributesCriteria();
+   }
+
+
+    protected void setUpStudyCriteria() {
+        StudyCriteria studyCrit = new StudyCriteria();
+        studyCrit.setName("CGEMS Prostate Scan 1");
+        //studyCrit.setSponsorStudyIdentifier("NIH");
+        spCrit.setStudyCriteria(studyCrit);
+    }
+
+
+
+    public void testGenotypeFindingCriteriaDTO() {
+        // 1. setup Annotation Criteria
+        //setUpSNPPhysicalPositionCrit();
+        //setUpDBSnpCrit();
+        setUpGeneBiomarkerCrit();
+
+
+        // 2. setup StudyParticipant Criteria
+        //setUpStudyParticipantCrit();
+        //setUpStudyParticipantAttributesCriteria();
+        setUpPopulationCriteria();
+        setUpStudyCriteria();
+   //     setUpAnalysisGroupCriteria();
+
+        // 3. set up Genotype Crit itself
+        setUpGenotypeCrit();
+
+        // 4. execute search
+        executeGenotypeFindingSearch(0, 501);
+    }
+
+
+
+
+    private void executeGenotypeFindingSearch(int startIndex, int endIndex) {
+       try {
+           Collection<? extends Finding> findings =
+                FindingsManager.getFindings(gfDTO, startIndex, endIndex);
+
+           System.out.println("RESULTS COUNT: " + findings.size());
+
+           for (Iterator<? extends Finding> iterator = findings.iterator(); iterator.hasNext();) {
+               GenotypeFinding finding =  (GenotypeFinding)iterator.next();
+               System.out.println("Fact ID:      " + finding.getId());
+               printSNPAnnotation(finding.getSnpAnnotation());
+
+               System.out.println(" Specimen ID: " + finding.getSpecimen().getId());
+               System.out.println(" Material Type: " + finding.getSpecimen().getMaterialType());
+               System.out.println("Study Partiicpant DE-Identifier Participant ID:  " + finding.getSpecimen().getStudyParticipant().getId());
+               System.out.println("QualityScore: " + finding.getQualityScore());
+               System.out.println("QC Status:       " + finding.getStatus());
+               System.out.println("Allel1:       " + finding.getAllele1());
+               System.out.println("Allel2:       " + finding.getAllele2());
+               System.out.println("Normalized X-intensity: " + finding.getNormalizedXIntensity() );
+               System.out.println("Normalized Y-intensity: " + finding.getNormalizedYIntensity() );
+               System.out.println("Raw X-intensity: " + finding.getRawXIntensity() );
+               System.out.println("Raw Y-intensity: " + finding.getRawYIntensity() );
+               //printSNPAnnotation(finding.getSnpAnnotation());
+               //System.out.println("DE-ID: " + finding.getSpecimen().getStudyParticipant().getId());
+           }
+
+       } catch (Throwable t)  {
+           System.out.println("CGEMS Exception: ");
+           t.printStackTrace();
+       }
+    }
+
+    protected Collection executeSearch(int start, int end) {
+       try {
+        Collection<? extends Finding> findings =
+                FindingsManager.getFindings(gfDTO, start, end);
+        System.out.println("RESULTS COUNT: " + findings.size());
+
+           /*for (Iterator<? extends Finding> iterator = findings.iterator(); iterator.hasNext();) {
+               SpecimenBasedMolecularFinding finding =  (SpecimenBasedMolecularFinding)iterator.next();
+               System.out.println("Specimen ID: " + finding.getSpecimen().getId());
+           }*/
+         return findings;
+       } catch (Throwable t)  {
+           System.out.println("CGEMS Exception: ");
+           t.printStackTrace();
+       }
+        return null;
+    }
+
+    public static Test suite() {
+        TestSuite suit =  new TestSuite();
+        suit.addTest(new TestSuite(GenotypeFindingTest.class));
+        return suit;
+    }
+
+    public static void main (String[] args) {
+        junit.textui.TestRunner.run(suite());
+
+    }
+}
