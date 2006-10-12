f31beb6d3d14cc7e715343f379df7bf642dc31c4
no_author <no_author@no_author.cvs2svn>
2006-10-12 16:29:13 +0000

    This commit was manufactured by cvs2svn to create tag 'caintegrator-spec_1_0_0_0_build-dependency_build_44'.
    SVN-Revision: 6980

---
+++ caintegrator-spec_1_0_0_0_build-dependency_build_44/src/gov/nih/nci/caintegrator/studyQueryService/dto/germline/AnalysisGroupCriteria.java
@@ -0,0 +1,37 @@
+package gov.nih.nci.caintegrator.studyQueryService.dto.germline;
+
+/**
+ /**
+  * Author: Ram Bhattaru
+  * Date:   Jul 21, 2006
+  * Time:   5:08:50 PM
+  */
+public class AnalysisGroupCriteria {
+
+    private String[] names;
+	public AnalysisGroupCriteria(){}
+
+    public String[] getNames() {
+        return names;
+    }
+
+    public void setNames(String[] names) {
+        this.names = names;
+    }
+
+	@Override
+	public String toString()
+	{
+		String str = "Analysis Groups\n";
+		
+		if ((names != null) && (names.length > 0))
+		{
+			for (String name : names)
+			{
+				str = str + name + "\n";
+			}
+		}
+			
+		return str;
+	}
+}
