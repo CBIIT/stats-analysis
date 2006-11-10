96ce4d1d7c2415768588be4478617376cbc4b37d
no_author <no_author@no_author.cvs2svn>
2006-11-10 22:57:55 +0000

    This commit was manufactured by cvs2svn to create tag 'ISPYPORTAL_DEV_12NOV06'.
    SVN-Revision: 7021

---
+++ ISPYPORTAL_DEV_12NOV06/src/gov/nih/nci/caintegrator/studyQueryService/dto/study/PopulationCriteria.java
@@ -0,0 +1,59 @@
+package gov.nih.nci.caintegrator.studyQueryService.dto.study;
+
+import java.util.Collection;
+import java.util.ArrayList;
+
+/**
+ * User: Ram Bhattaru
+ * Date: Jul 3, 2006
+ * Time: 5:21:49 PM
+*/
+/**
+ * Groups of subjects based on self-described ethnic groupings and phenotypic
+ * ascertainment scheme.
+ */
+
+public class PopulationCriteria {
+
+	/**
+	 * A textual identifier for the study population
+	 */
+	private Collection<String> names;
+
+    public PopulationCriteria() {
+        names = new ArrayList<String>();
+    }
+
+    public void addNames(Collection<String> names) {
+        this.names.addAll(names);
+    }
+
+    public PopulationCriteria(Collection<String> names) {
+        this.names = names;
+    }
+
+    public Collection<String> getNames() {
+        return names;
+    }
+
+    public void setNames(Collection<String> names) {
+        this.names = names;
+    }
+
+	@Override
+	public String toString()
+	{
+		String str = "Population Criteria\n";
+		
+		if ((names != null) && (names.size() > 0))
+		{
+			str = str + "Populations:\n";
+			for (String name : names)
+			{
+				str = str + name + "\n";
+			}
+		}
+		
+		return str;
+	}
+}
