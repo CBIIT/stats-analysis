6c5e6467d16aad6f515ec7ce57944896dc0a0d86
Kevin Rosso <Kevin.Rosso@fda.hhs.gov>
2006-11-17 17:50:44 +0000

    *** empty log message ***
    SVN-Revision: 7047

--- src/gov/nih/nci/caintegrator/domain/finding/protein/ihc/bean/LossOfExpressionIHCFinding.java
+++ src/gov/nih/nci/caintegrator/domain/finding/protein/ihc/bean/LossOfExpressionIHCFinding.java
@@ -8,6 +8,86 @@ public class LossOfExpressionIHCFinding extends IHCFinding implements Serializab
 	 * 
 	 */
 	private static final long serialVersionUID = 3689069021034505228L;
+    
+    private String benignPresentValue;
+    private Integer benignSum;
+    private Integer invasiveSum;
+    private Integer invasiveBenignDiff;
+    private String lossResult;
+    private String comments;
+    /**
+     * @return Returns the benignPresentValue.
+     */
+    public String getBenignPresentValue() {
+        return benignPresentValue;
+    }
+    /**
+     * @param benignPresentValue The benignPresentValue to set.
+     */
+    public void setBenignPresentValue(String benignPresentValue) {
+        this.benignPresentValue = benignPresentValue;
+    }
+    /**
+     * @return Returns the benignSum.
+     */
+    public Integer getBenignSum() {
+        return benignSum;
+    }
+    /**
+     * @param benignSum The benignSum to set.
+     */
+    public void setBenignSum(Integer benignSum) {
+        this.benignSum = benignSum;
+    }
+    /**
+     * @return Returns the comments.
+     */
+    public String getComments() {
+        return comments;
+    }
+    /**
+     * @param comments The comments to set.
+     */
+    public void setComments(String comments) {
+        this.comments = comments;
+    }
+    /**
+     * @return Returns the invasiveBenignDiff.
+     */
+    public Integer getInvasiveBenignDiff() {
+        return invasiveBenignDiff;
+    }
+    /**
+     * @param invasiveBenignDiff The invasiveBenignDiff to set.
+     */
+    public void setInvasiveBenignDiff(Integer invasiveBenignDiff) {
+        this.invasiveBenignDiff = invasiveBenignDiff;
+    }
+    /**
+     * @return Returns the invasiveSum.
+     */
+    public Integer getInvasiveSum() {
+        return invasiveSum;
+    }
+    /**
+     * @param invasiveSum The invasiveSum to set.
+     */
+    public void setInvasiveSum(Integer invasiveSum) {
+        this.invasiveSum = invasiveSum;
+    }
+    /**
+     * @return Returns the lossResult.
+     */
+    public String getLossResult() {
+        return lossResult;
+    }
+    /**
+     * @param lossResult The lossResult to set.
+     */
+    public void setLossResult(String lossResult) {
+        this.lossResult = lossResult;
+    }
+    
 	
 	
 
