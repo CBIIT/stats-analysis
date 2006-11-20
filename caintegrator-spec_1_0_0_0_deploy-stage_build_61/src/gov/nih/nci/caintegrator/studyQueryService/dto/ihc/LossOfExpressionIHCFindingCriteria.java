923c2e8f0ad6598fd4fb7593ef6f67cd3c5e0c97
no_author <no_author@no_author.cvs2svn>
2006-11-20 15:07:40 +0000

    This commit was manufactured by cvs2svn to create tag 'caintegrator-spec_1_0_0_0_deploy-stage_build_61'.
    SVN-Revision: 7058

---
+++ caintegrator-spec_1_0_0_0_deploy-stage_build_61/src/gov/nih/nci/caintegrator/studyQueryService/dto/ihc/LossOfExpressionIHCFindingCriteria.java
@@ -0,0 +1,74 @@
+package gov.nih.nci.caintegrator.studyQueryService.dto.ihc;
+
+import gov.nih.nci.caintegrator.dto.query.QueryDTO;
+import gov.nih.nci.caintegrator.studyQueryService.ihc.LossOfExpressionIHCFindingHandler;
+
+import java.util.Collection;
+
+public class LossOfExpressionIHCFindingCriteria extends IHCFindingCriteria implements QueryDTO {
+	/*public SpecimenBasedMolecularFindingHandler getHandler() {		
+		return new LevelOfExpressionIHCFindingHandler();
+	}*/
+	
+	public LossOfExpressionIHCFindingHandler getHandler() {		
+		return new LossOfExpressionIHCFindingHandler();
+	}
+	
+	private Collection <String> stainIntensityCollection;
+	private Collection <String> stainLocalizationCollection;
+	private Collection <String> stainDistributionCollection;
+	private Integer percentPositive;
+	private Integer percentPositiveRangeMin;
+	private Integer percentPositiveRangeMax;
+    private String queryName;
+	
+	
+	public Integer getPercentPositive() {
+		return percentPositive;
+	}
+	public void setPercentPositive(Integer percentPositive) {
+		this.percentPositive = percentPositive;
+	}
+	public Integer getPercentPositiveRangeMax() {
+		return percentPositiveRangeMax;
+	}
+	public void setPercentPositiveRangeMax(Integer percentPositiveRangeMax) {
+		this.percentPositiveRangeMax = percentPositiveRangeMax;
+	}
+	public Integer getPercentPositiveRangeMin() {
+		return percentPositiveRangeMin;
+	}
+	public void setPercentPositiveRangeMin(Integer percentPositiveRangeMin) {
+		this.percentPositiveRangeMin = percentPositiveRangeMin;
+	}
+	public Collection<String> getStainIntensityCollection() {
+		return stainIntensityCollection;
+	}
+	public void setStainIntensityCollection(
+			Collection<String> stainIntensityCollection) {
+		this.stainIntensityCollection = stainIntensityCollection;
+	}
+	public Collection<String> getStainLocalizationCollection() {
+		return stainLocalizationCollection;
+	}
+	public void setStainLocalizationCollection(
+			Collection<String> stainLocalizationCollection) {
+		this.stainLocalizationCollection = stainLocalizationCollection;
+	}
+	public Collection<String> getStainDistributionCollection() {
+		return stainDistributionCollection;
+	}
+	public void setStainDistributionCollection(Collection<String> stainDistributionCollection) {
+		this.stainDistributionCollection = stainDistributionCollection;
+	}
+    public void setQueryName(String name) {
+        this.queryName = name;        
+    }
+    public String getQueryName() {
+        return queryName;
+    }
+	
+	
+	
+
+}
