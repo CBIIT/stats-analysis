7c7057a3426368d5c1921f6094ea9cd8ceca8140
no_author <no_author@no_author.cvs2svn>
2006-12-12 17:42:19 +0000

    This commit was manufactured by cvs2svn to create tag 'ISPY_VER1P0_RC7_13DEC06'.
    SVN-Revision: 7169

---
+++ ISPY_VER1P0_RC7_13DEC06/src/gov/nih/nci/caintegrator/studyQueryService/dto/study/StudyCriteria.java
@@ -0,0 +1,54 @@
+package gov.nih.nci.caintegrator.studyQueryService.dto.study;
+
+/**
+ * User: Ram Bhattaru
+ * Date: Jul 3, 2006
+ * Time: 5:21:49 PM
+*/
+/**
+ * A type of research activity that tests how well new medical treatments or other
+ * interventions work in subjects. Such plans test new methods of screening,
+ * prevention, diagnosis or treatment of a disease. The specific plans are fully
+ * defined in the protocol and may be carried out in a clinic or other medical
+ * facility.
+ */
+
+public class StudyCriteria {
+
+	private String name;
+	/**
+	 * The unique identifier for the study assigned by the study sponsoring
+	 * organization.
+	 */
+	private String sponsorStudyIdentifier;
+
+	public StudyCriteria(){}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSponsorStudyIdentifier() {
+        return sponsorStudyIdentifier;
+    }
+
+    public void setSponsorStudyIdentifier(String sponsorStudyIdentifier) {
+        this.sponsorStudyIdentifier = sponsorStudyIdentifier;
+    }
+    
+    @Override
+	public String toString()
+    {
+		String str = "Study Criteria\n";
+		
+		if (name != null)
+			str = str + "Study Name: " + name + "\n";
+		
+		return str;
+	}
+
+}
