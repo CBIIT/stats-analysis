448f79491901cde815524f089127c3351d2f69f4
no_author <no_author@no_author.cvs2svn>
2007-03-02 20:46:59 +0000

    This commit was manufactured by cvs2svn to create tag 'QA_CGEMS_05MAR07_01'.
    SVN-Revision: 7361

---
+++ QA_CGEMS_05MAR07_01/src/gov/nih/nci/caintegrator/domain/annotation/snp/bean/VariationReporter.java
@@ -0,0 +1,26 @@
+
+
+package gov.nih.nci.caintegrator.domain.annotation.snp.bean;
+import gov.nih.nci.caintegrator.domain.common.bean.Reporter;
+
+/**
+ * <!-- LICENSE_TEXT_START -->
+ * <!-- LICENSE_TEXT_END -->
+ */
+ 
+  /**
+   * Variation reporter is a design element which is used to report change (variation) - alteration, 
+   * deletion, or rearrangement - in the DNA sequence . 
+   * 
+   */
+
+public  class VariationReporter  extends Reporter
+
+
+	implements java.io.Serializable 
+{
+
+	private static final long serialVersionUID = 1234567890L;
+
+			
+}
