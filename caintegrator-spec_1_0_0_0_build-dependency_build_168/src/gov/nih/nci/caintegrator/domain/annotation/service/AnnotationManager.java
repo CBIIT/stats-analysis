54f55bd434f8ecc42b5805381fb5a1363e78d96a
no_author <no_author@no_author.cvs2svn>
2007-03-30 20:11:25 +0000

    This commit was manufactured by cvs2svn to create tag 'caintegrator-spec_1_0_0_0_build-dependency_build_168'.
    SVN-Revision: 7444

---
+++ caintegrator-spec_1_0_0_0_build-dependency_build_168/src/gov/nih/nci/caintegrator/domain/annotation/service/AnnotationManager.java
@@ -0,0 +1,34 @@
+package gov.nih.nci.caintegrator.domain.annotation.service;
+
+import gov.nih.nci.caintegrator.domain.annotation.gene.bean.CytobandPosition;
+import gov.nih.nci.caintegrator.domain.annotation.gene.bean.GeneAlias;
+import gov.nih.nci.caintegrator.domain.annotation.gene.bean.GeneBiomarker;
+import gov.nih.nci.caintegrator.domain.annotation.gene.bean.GeneExprReporter;
+import gov.nih.nci.caintegrator.domain.annotation.snp.bean.SNPAnnotation;
+import gov.nih.nci.caintegrator.domain.annotation.snp.bean.VariationReporter;
+import gov.nih.nci.caintegrator.studyQueryService.dto.annotation.AnnotationCriteria;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+public interface AnnotationManager {
+
+    public Collection<GeneExprReporter> getGenesForReporters(
+            AnnotationCriteria criteria);
+
+    public Map<GeneBiomarker, Collection<GeneExprReporter>> getReportersForGenes(
+            AnnotationCriteria criteria);
+    
+    public GeneBiomarker getGeneForSymbol(String geneId);
+    
+    public List<GeneAlias> getGeneAliasForSymbol(String symbol);
+    
+    public List<CytobandPosition> getCytobandPositions(String chromosome, String startCytoband, String endCytoband);
+    
+    public List<CytobandPosition> getCytobandPositions(String chromosome);
+    
+    public List<VariationReporter> getReportersForDbSnpId(String dbSnpId);
+    
+    public List<SNPAnnotation> getSnpAnnotationsForGene(String geneId, Long kbUpstream, Long kbDownstream);
+}
