5478dd2e989429a0e5a749e55070a55195f086b2
no_author <no_author@no_author.cvs2svn>
2007-07-26 13:30:30 +0000

    This commit was manufactured by cvs2svn to create tag 'ISPY_GP_DEV_26JULY2007'.
    SVN-Revision: 7607

---
+++ ISPY_GP_DEV_26JULY2007/src/gov/nih/nci/caintegrator/domain/analysis/snp/bean/SNPAnalysisMethod.java
@@ -0,0 +1,104 @@
+/**
+ * 
+ */
+package gov.nih.nci.caintegrator.domain.analysis.snp.bean;
+
+import gov.nih.nci.caintegrator.domain.study.bean.Study;
+
+/**
+ * @author sahnih
+ *
+ */
+public class SNPAnalysisMethod {
+	private Long id;
+	private String methodDescription;
+	private String methodName;
+	private String methodType;
+	private String represensitiveCode;
+	private Study study;
+	private Long displayOrder;
+	/**
+	 * @return Returns the id.
+	 */
+	public Long getId() {
+		return id;
+	}
+	/**
+	 * @param id The id to set.
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+	/**
+	 * @return Returns the methodDescription.
+	 */
+	public String getMethodDescription() {
+		return methodDescription;
+	}
+	/**
+	 * @param methodDescription The methodDescription to set.
+	 */
+	public void setMethodDescription(String methodDescription) {
+		this.methodDescription = methodDescription;
+	}
+	/**
+	 * @return Returns the methodName.
+	 */
+	public String getMethodName() {
+		return methodName;
+	}
+	/**
+	 * @param methodName The methodName to set.
+	 */
+	public void setMethodName(String methodName) {
+		this.methodName = methodName;
+	}
+	/**
+	 * @return Returns the methodType.
+	 */
+	public String getMethodType() {
+		return methodType;
+	}
+	/**
+	 * @param methodType The methodType to set.
+	 */
+	public void setMethodType(String methodType) {
+		this.methodType = methodType;
+	}
+	/**
+	 * @return Returns the represensitiveCode.
+	 */
+	public String getRepresensitiveCode() {
+		return represensitiveCode;
+	}
+	/**
+	 * @param represensitiveCode The represensitiveCode to set.
+	 */
+	public void setRepresensitiveCode(String represensitiveCode) {
+		this.represensitiveCode = represensitiveCode;
+	}
+	/**
+	 * @return Returns the displayOrder.
+	 */
+	public Long getDisplayOrder() {
+		return displayOrder;
+	}
+	/**
+	 * @param displayOrder The displayOrder to set.
+	 */
+	public void setDisplayOrder(Long displayOrder) {
+		this.displayOrder = displayOrder;
+	}
+	/**
+	 * @return Returns the study.
+	 */
+	public Study getStudy() {
+		return study;
+	}
+	/**
+	 * @param study The study to set.
+	 */
+	public void setStudy(Study study) {
+		this.study = study;
+	}
+}
