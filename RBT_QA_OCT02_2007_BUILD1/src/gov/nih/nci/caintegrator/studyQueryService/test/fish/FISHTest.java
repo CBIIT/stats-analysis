055c3fc65d5ef0e5de6fa2e00d8bd686577c47ae
no_author <no_author@no_author.cvs2svn>
2007-09-17 15:17:03 +0000

    This commit was manufactured by cvs2svn to create tag 'RBT_QA_OCT02_2007_BUILD1'.
    SVN-Revision: 7662

---
+++ RBT_QA_OCT02_2007_BUILD1/src/gov/nih/nci/caintegrator/studyQueryService/test/fish/FISHTest.java
@@ -0,0 +1,84 @@
+package gov.nih.nci.caintegrator.studyQueryService.test.fish;
+
+import gov.nih.nci.caintegrator.domain.finding.copyNumber.fish.bean.FISHFinding;
+import gov.nih.nci.caintegrator.studyQueryService.dto.fish.FISHFindingCriteria;
+import gov.nih.nci.caintegrator.studyQueryService.fish.FISHFindingHandler;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+
+public class FISHTest extends TestCase {
+	
+   private FISHFindingCriteria inCriteria;
+   
+   public void setUp() throws Exception{
+	   inCriteria = new FISHFindingCriteria();   
+	   }
+	
+   public static Test suite() {
+	    TestSuite suit =  new TestSuite();	        
+	    suit.addTest(new TestSuite(FISHTest.class));
+	    return suit;
+	  }
+   public void testFISHSearch() {
+	   setUpFISHAttributesCriteria();       
+       executeSearch();
+   }  
+   
+  
+   private void setUpFISHAttributesCriteria() {
+	   inCriteria = new FISHFindingCriteria();
+	   Set<String> biomarkers = new HashSet<String>();
+	   biomarkers.add("ERBB2");
+	   inCriteria.setBioMarkerCollection(biomarkers);      
+   }
+   
+   private  Collection executeSearch() {
+    	
+    	 try {
+             System.setProperty("gov.nih.nci.caintegrator.configFile","C:/devtools/jboss/jboss-4.0.4.GA/server/default/conf/caIntegratorConfig.xml");
+    		 FISHFindingHandler fishHandler = new FISHFindingHandler();
+             Collection<FISHFinding> fish = fishHandler.getFISHFindings(inCriteria);             
+             System.out.println("Number of fish finding Retrieved: " + fish.size());
+             
+             for (Iterator<FISHFinding> iterator = fish.iterator(); iterator.hasNext();) {
+            	 FISHFinding fishFinding =  iterator.next();
+                 System.out.println("FISH finding ID: " +
+                		             fishFinding.getId());
+                 System.out.println("GENE: " + fishFinding.getGene());
+                 System.out.println("abnomality status: " + fishFinding.getAbnomalityStatus());
+                 System.out.println("ratio:  " + fishFinding.getRatio());
+                 System.out.println("patient did: " + fishFinding.getSpecimen().getPatientDID());
+                 System.out.println("specimen id: "+ fishFinding.getSpecimen().getId());
+                 if (fishFinding.getSpecimen().getTimePoint() != null)
+                     System.out.println("time point:" + fishFinding.getSpecimen().getTimePoint());
+             }
+             return fish;
+         } catch (Throwable t)  {
+            System.out.println("FISH Exception in getting data: " + t.toString());
+            t.printStackTrace();
+        }
+         
+    	return null;
+    }   
+    
+ 
+    public void testAll() {       
+        Collection findings =  executeSearch();
+    }    
+    
+    
+    public static void main (String[] args) {
+        junit.textui.TestRunner.run(suite());
+
+    }
+
+
+}
