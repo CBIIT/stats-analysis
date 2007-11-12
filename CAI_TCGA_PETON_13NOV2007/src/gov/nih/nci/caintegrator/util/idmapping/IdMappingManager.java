f06f4162ca4480b8c41e37308afc004de50eb49c
no_author <no_author@no_author.cvs2svn>
2007-11-12 19:58:23 +0000

    This commit was manufactured by cvs2svn to create tag 'CAI_TCGA_PETON_13NOV2007'.
    SVN-Revision: 7759

---
+++ CAI_TCGA_PETON_13NOV2007/src/gov/nih/nci/caintegrator/util/idmapping/IdMappingManager.java
@@ -0,0 +1,75 @@
+package gov.nih.nci.caintegrator.util.idmapping;
+
+import gov.nih.nci.caintegrator.domain.annotation.gene.bean.GeneExprReporter;
+
+import java.util.List;
+import java.util.Set;
+import java.util.HashSet;
+
+import org.hibernate.Criteria;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.CriteriaSpecification;
+import org.hibernate.criterion.Disjunction;
+import org.hibernate.criterion.Expression;
+import org.hibernate.criterion.MatchMode;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Restrictions;
+
+public class IdMappingManager implements IdMapper{
+
+	SessionFactory sessionFactory;
+	
+	public Set getRbinaryIdsForPatientDIDs(IdMappingCriteria criteria) {
+		
+		Session currentSession = sessionFactory.getCurrentSession();
+		  Criteria c = currentSession
+          .createCriteria(IdMapping.class);
+          c.add(Restrictions.in("pdid", criteria.getPatientIds()));
+          c.add(Restrictions.eq("fileName", criteria.getFileName()));
+          List<IdMapping> objs = c.list();
+          Set<String> rbinaryIds = new HashSet<String>();
+          for (IdMapping idm : objs) {
+             rbinaryIds.add(idm.getAnalysisFileId());
+          }
+          
+          return rbinaryIds;
+	}
+	
+	
+	public SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
+
+	public void setSessionFactory(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+	}
+
+    
+
+
+    public Set getPatientDIDsForAnalysisIds(IdMappingCriteria criteria) {
+        Session currentSession = sessionFactory.getCurrentSession();
+        Criteria c = currentSession
+        .createCriteria(IdMapping.class);
+        c.add(Restrictions.in("analysisFileId", criteria.getAnalysisFileIds()));
+        c.add(Restrictions.eq("fileName", criteria.getFileName()));
+        List<IdMapping> objs = c.list();
+        Set<String> patientIds = new HashSet<String>();
+        for (IdMapping idm : objs) {
+            patientIds.add(idm.getPdid());
+        }
+        
+        return patientIds;
+  }
+    
+
+
+    public Set getRbinaryIdsForPatientDIDs(Set patientDIDs, String tissue,
+            String binaryFileName) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+	
+}
