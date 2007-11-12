e9b90e9f854cfe7af5f63fbeadbd3095dfae23dc
Kevin Rosso <Kevin.Rosso@fda.hhs.gov>
2007-11-12 16:11:39 +0000

    added new method
    SVN-Revision: 7752

--- src/gov/nih/nci/caintegrator/util/idmapping/NullIdMappingManager.java
+++ src/gov/nih/nci/caintegrator/util/idmapping/NullIdMappingManager.java
@@ -48,4 +48,9 @@ public class NullIdMappingManager implements IdMapper {
 
     }
 
+    public Set getPatientDIDsForAnalysisIds(IdMappingCriteria criteria) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
 }
