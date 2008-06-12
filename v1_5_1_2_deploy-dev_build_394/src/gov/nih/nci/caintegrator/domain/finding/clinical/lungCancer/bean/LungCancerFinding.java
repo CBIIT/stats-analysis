66b293ca160d477e4f75804db22dc8c0b0da3e81
no_author <no_author@no_author.cvs2svn>
2008-06-12 16:15:10 +0000

    This commit was manufactured by cvs2svn to create tag 'v1_5_1_2_deploy-dev_build_394'.
    SVN-Revision: 7978

---
+++ v1_5_1_2_deploy-dev_build_394/src/gov/nih/nci/caintegrator/domain/finding/clinical/lungCancer/bean/LungCancerFinding.java
@@ -0,0 +1,411 @@
+
+
+package gov.nih.nci.caintegrator.domain.finding.clinical.lungCancer.bean;
+import gov.nih.nci.caintegrator.domain.finding.clinical.lungCancer.bean.*;
+import java.util.*;
+
+/**
+ * <!-- LICENSE_TEXT_START -->
+ * <!-- LICENSE_TEXT_END -->
+ */
+ 
+  /**
+
+   */
+
+public  class LungCancerFinding 
+    extends gov.nih.nci.caintegrator.domain.finding.bean.SpecimenBasedFinding
+
+
+	implements java.io.Serializable 
+{
+
+	private static final long serialVersionUID = 1234567890L;
+
+
+ 	
+	   
+    /**
+   * DOCUMENT ME!
+   */
+
+    private java.lang.Boolean Frozen_Acinar;
+    /**
+   * DOCUMENT ME!
+   */
+
+	public  java.lang.Boolean getFrozen_Acinar(){
+        return Frozen_Acinar;
+    }
+    public void setFrozen_Acinar( java.lang.Boolean Frozen_Acinar){
+        this.Frozen_Acinar = Frozen_Acinar;
+    }
+	
+	   
+    /**
+   * DOCUMENT ME!
+   */
+
+    private java.lang.Boolean Frozen_Papillary;
+    /**
+   * DOCUMENT ME!
+   */
+
+	public  java.lang.Boolean getFrozen_Papillary(){
+        return Frozen_Papillary;
+    }
+    public void setFrozen_Papillary( java.lang.Boolean Frozen_Papillary){
+        this.Frozen_Papillary = Frozen_Papillary;
+    }
+	
+	   
+    /**
+   * DOCUMENT ME!
+   */
+
+    private java.lang.Boolean Frozen_Concordant;
+    /**
+   * DOCUMENT ME!
+   */
+
+	public  java.lang.Boolean getFrozen_Concordant(){
+        return Frozen_Concordant;
+    }
+    public void setFrozen_Concordant( java.lang.Boolean Frozen_Concordant){
+        this.Frozen_Concordant = Frozen_Concordant;
+    }
+	
+	   
+    /**
+   * DOCUMENT ME!
+   */
+
+    private java.lang.String Frozen_BAC;
+    /**
+   * DOCUMENT ME!
+   */
+
+	public  java.lang.String getFrozen_BAC(){
+        return Frozen_BAC;
+    }
+    public void setFrozen_BAC( java.lang.String Frozen_BAC){
+        this.Frozen_BAC = Frozen_BAC;
+    }
+	
+	   
+    /**
+   * DOCUMENT ME!
+   */
+
+    private java.lang.Boolean Frozen_Micropapillary;
+    /**
+   * DOCUMENT ME!
+   */
+
+	public  java.lang.Boolean getFrozen_Micropapillary(){
+        return Frozen_Micropapillary;
+    }
+    public void setFrozen_Micropapillary( java.lang.Boolean Frozen_Micropapillary){
+        this.Frozen_Micropapillary = Frozen_Micropapillary;
+    }
+	
+	   
+    /**
+   * DOCUMENT ME!
+   */
+
+    private java.lang.Integer Percent_Tumor;
+    /**
+   * DOCUMENT ME!
+   */
+
+	public  java.lang.Integer getPercent_Tumor(){
+        return Percent_Tumor;
+    }
+    public void setPercent_Tumor( java.lang.Integer Percent_Tumor){
+        this.Percent_Tumor = Percent_Tumor;
+    }
+	
+	   
+    /**
+   * DOCUMENT ME!
+   */
+
+    private java.lang.Boolean Frozen_Solid;
+    /**
+   * DOCUMENT ME!
+   */
+
+	public  java.lang.Boolean getFrozen_Solid(){
+        return Frozen_Solid;
+    }
+    public void setFrozen_Solid( java.lang.Boolean Frozen_Solid){
+        this.Frozen_Solid = Frozen_Solid;
+    }
+	
+	   
+    /**
+   * DOCUMENT ME!
+   */
+
+    private Float ClearCell;
+    /**
+   * DOCUMENT ME!
+   */
+
+	public Float getClearCell(){
+        return ClearCell;
+    }
+    public void setClearCell(Float ClearCell){
+        this.ClearCell = ClearCell;
+    }
+	
+	   
+    /**
+   * DOCUMENT ME!
+   */
+
+    private Float Fetal;
+    /**
+   * DOCUMENT ME!
+   */
+
+	public Float getFetal(){
+        return Fetal;
+    }
+    public void setFetal(Float Fetal){
+        this.Fetal = Fetal;
+    }
+	
+	   
+    /**
+   * DOCUMENT ME!
+   */
+
+    private java.lang.Float Signet_Ring;
+    /**
+   * DOCUMENT ME!
+   */
+
+	public  java.lang.Float getSignet_Ring(){
+        return Signet_Ring;
+    }
+    public void setSignet_Ring( java.lang.Float Signet_Ring){
+        this.Signet_Ring = Signet_Ring;
+    }
+	
+	   
+    /**
+   * DOCUMENT ME!
+   */
+
+    private java.lang.Integer Papillary;
+    /**
+   * DOCUMENT ME!
+   */
+
+	public  java.lang.Integer getPapillary(){
+        return Papillary;
+    }
+    public void setPapillary( java.lang.Integer Papillary){
+        this.Papillary = Papillary;
+    }
+	
+	   
+    /**
+   * DOCUMENT ME!
+   */
+
+    private java.lang.Integer Acinar;
+    /**
+   * DOCUMENT ME!
+   */
+
+	public  java.lang.Integer getAcinar(){
+        return Acinar;
+    }
+    public void setAcinar( java.lang.Integer Acinar){
+        this.Acinar = Acinar;
+    }
+	
+	   
+    /**
+   * DOCUMENT ME!
+   */
+
+    private java.lang.Integer MicroPapillary;
+    /**
+   * DOCUMENT ME!
+   */
+
+	public  java.lang.Integer getMicroPapillary(){
+        return MicroPapillary;
+    }
+    public void setMicroPapillary( java.lang.Integer MicroPapillary){
+        this.MicroPapillary = MicroPapillary;
+    }
+	
+	   
+    /**
+   * DOCUMENT ME!
+   */
+
+    private java.lang.Integer Colloid;
+    /**
+   * DOCUMENT ME!
+   */
+
+	public  java.lang.Integer getColloid(){
+        return Colloid;
+    }
+    public void setColloid( java.lang.Integer Colloid){
+        this.Colloid = Colloid;
+    }
+	
+	   
+    /**
+   * DOCUMENT ME!
+   */
+
+    private java.lang.Integer Solid;
+    /**
+   * DOCUMENT ME!
+   */
+
+	public  java.lang.Integer getSolid(){
+        return Solid;
+    }
+    public void setSolid( java.lang.Integer Solid){
+        this.Solid = Solid;
+    }
+	
+	   
+    /**
+   * DOCUMENT ME!
+   */
+
+    private java.lang.Integer BAC;
+    /**
+   * DOCUMENT ME!
+   */
+
+	public  java.lang.Integer getBAC(){
+        return BAC;
+    }
+    public void setBAC( java.lang.Integer BAC){
+        this.BAC = BAC;
+    }
+	
+	   
+    /**
+   * DOCUMENT ME!
+   */
+
+    private java.lang.String BAC_Type;
+    /**
+   * DOCUMENT ME!
+   */
+
+	public  java.lang.String getBAC_Type(){
+        return BAC_Type;
+    }
+    public void setBAC_Type( java.lang.String BAC_Type){
+        this.BAC_Type = BAC_Type;
+    }
+	
+	   
+    /**
+   * DOCUMENT ME!
+   */
+
+    private java.lang.String Pahology_DX;
+    /**
+   * DOCUMENT ME!
+   */
+
+	public  java.lang.String getPahology_DX(){
+        return Pahology_DX;
+    }
+    public void setPahology_DX( java.lang.String Pahology_DX){
+        this.Pahology_DX = Pahology_DX;
+    }
+	
+	   
+    /**
+   * DOCUMENT ME!
+   */
+
+    private java.lang.String Path_Review;
+    /**
+   * DOCUMENT ME!
+   */
+
+	public  java.lang.String getPath_Review(){
+        return Path_Review;
+    }
+    public void setPath_Review( java.lang.String Path_Review){
+        this.Path_Review = Path_Review;
+    }
+	
+	   
+    /**
+   * DOCUMENT ME!
+   */
+
+    private java.lang.Integer TSP_Normal_DNA_ID;
+    /**
+   * DOCUMENT ME!
+   */
+
+	public  java.lang.Integer getTSP_Normal_DNA_ID(){
+        return TSP_Normal_DNA_ID;
+    }
+    public void setTSP_Normal_DNA_ID( java.lang.Integer TSP_Normal_DNA_ID){
+        this.TSP_Normal_DNA_ID = TSP_Normal_DNA_ID;
+    }
+	
+	   
+    /**
+   * DOCUMENT ME!
+   */
+
+    private java.lang.String Normal_DNA_Source;
+    /**
+   * DOCUMENT ME!
+   */
+
+	public  java.lang.String getNormal_DNA_Source(){
+        return Normal_DNA_Source;
+    }
+    public void setNormal_DNA_Source( java.lang.String Normal_DNA_Source){
+        this.Normal_DNA_Source = Normal_DNA_Source;
+    }
+	
+
+
+		public boolean equals(Object obj){
+			boolean eq = false;
+			if(obj instanceof LungCancerFinding) {
+				LungCancerFinding c =(LungCancerFinding)obj; 			 
+				Long thisId = getId();		
+				
+					if(thisId != null && thisId.equals(c.getId())) {
+					   eq = true;
+				    }		
+				
+			}
+			return eq;
+		}
+		
+		public int hashCode(){
+			int h = 0;
+			
+			if(getId() != null) {
+				h += getId().hashCode();
+			}
+			
+			return h;
+	}
+
+			
+}
