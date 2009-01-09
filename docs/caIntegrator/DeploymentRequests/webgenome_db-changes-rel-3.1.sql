58e0bd0ab1fbb896aa5465df75ff7266e09b21c7
Leonie Misquitta <leonie.misquitta@nih.gov>
2009-01-09 16:06:37 +0000

    Build, deployment and database documents for Rembrandt and Webgenome on stage and production.
    SVN-Revision: 9723

--- docs/caIntegrator/DeploymentRequests/webgenome_db-changes-rel-3.1.sql
+++ docs/caIntegrator/DeploymentRequests/webgenome_db-changes-rel-3.1.sql
@@ -10,5 +10,5 @@ ALTER TABLE principal ADD (address VARCHAR2(400) NULL);
 ALTER TABLE principal ADD (is_feedbacks CHAR(1) NULL);
 
 
-ALTER TABLE job ADD(params varchar(255) NULL)
+ALTER TABLE job ADD(params varchar(255) NULL);
 ALTER TABLE job ADD (bioassay_id NUMBER NULL);
